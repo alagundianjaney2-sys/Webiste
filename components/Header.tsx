70f84359cffe05a9161478d3f649cc22674d5e5f
Vijay-konakeri <vijay.bk698@gmail.com>
2025-12-08 10:51:18 +0530

    Reduce header vertical padding by 50% - py-2 to py-1

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
 
   return (
     <header className="bg-white shadow-md sticky top-0 z-50">
-      <div className="container mx-auto px-4 py-2">
+      <div className="container mx-auto px-4 py-1">
         <div className="flex justify-between items-center">
           {/* Logo */}
           <Link href="/" className="flex items-center">
